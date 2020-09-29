53c7344b38feb3cd7d6a087d05a7ee8bbad0a84f
Fésű Gábor <fesugabor77@gmail.com>
2020-09-29 11:30:27 +0200

    dasdasdas

--- jelszo.py
+++ jelszo.py
@@ -16,7 +16,7 @@ def jelszo_spec(db):
 def jelszo_nagybetu(db):
 	szoveg3=""
 	tomb3="QWERTZUIOPASDFGHJKLYXCVBNM"
-for i in range (0,db):
+for x in range (0,db):
 	szoveg3+=tomb3[random.randint(0,len(abc)-1)]
 	return szoveg3
 
@@ -25,7 +25,7 @@ def jelszo_kisbetu(db)
 	szoveg4=""
 	tomb4="abcdefghijklmnopqrstuvwxyz"
 	for x in range (0,db)
-		szoveg4+=tomb4[random.randint(0,len(tomb))]:
+		szoveg4+=tomb4[random.randint(0,len(tomb))]
 	return szoveg4
 	
 jelszo=jelszo_szam(2)+jelszo_nagybetu(3)+jelszo_kisbetu(3)+jelszo_spec(2)+jelszo_szam(1)
