bf1d808bbe10a785ea4f01229d40d87145bd3f5c
Fésű Gábor <fesugabor77@gmail.com>
2020-09-29 10:50:29 +0200

    nagybetu

---
+++ jelszo_nagybetu.py
@@ -0,0 +1,8 @@
+import random
+def jelszo_nagybetu(db)
+abc="QWERTZUIOPASDFGHJKLYXCVBNM"
+jelszohossz=3
+jelszo=""
+for i in range (0,jelszohossz):
+	jelszo+=abc[random.randint(0,len(abc)-1)]
+print(jelszo)	
