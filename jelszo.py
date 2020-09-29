c320e2762736b8d6c5483bfd38ad1eb7ad88f473
djvalee <djvalee.yt@gmail.com>
2020-09-29 10:50:40 +0200

    spec karakterek

--- jelszo.py
+++ jelszo.py
@@ -8,3 +8,10 @@ def jelszo_szam(db):
 print (jelszo_szam(1))
 print (jelszo_szam(5))
 print (jelszo_szam(8))
+
+def jelszo_spec(db):
+	szoveg=""
+	tomb="*>;<@&#~"
+	for x in range (0,db):
+			szoveg+=tomb[random.randint(0,len(tomb))]
+	return szoveg		
