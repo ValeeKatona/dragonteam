19163ec0be5e74746c6ed6c692ad5860a691a81b
subiczadrian <mate.kollath@gesign.hu>
2020-09-29 11:06:31 +0200

    h

--- jelszo.py
+++ jelszo.py
@@ -1,10 +1,7 @@
 import random
-def jelszo_szam(db):
+def jelszo_szam(db)
 	szoveg=""
 	tomb="123456789"
 	for x in range (0,db)
 		szoveg+=tomb[random.randint(0,len(tomb))]:
 	return szoveg
-print (jelszo_szam(1))
-print (jelszo_szam(5))
-print (jelszo_szam(8))
